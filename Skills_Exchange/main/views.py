60662edf6102ca85bfb0d94af2277758d2007ec3
Umer Farooque <146421632+UmerF-exe@users.noreply.github.com>
2026-04-22 18:58:06 +0500

    Update views.py

--- Skills_Exchange/main/views.py
+++ Skills_Exchange/main/views.py
@@ -103,7 +103,8 @@ def logout_view(request):
 
 
 def index_view(request):
-    return render(request, "index.html")
+    # return render(request, "index.html")
+    return HttpResponse("WORKING 🚀")
 
 
 @login_required
